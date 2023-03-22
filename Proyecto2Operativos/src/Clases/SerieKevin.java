f6700556af932d7728e945e0e677c63ec4f1f881
“Kevin” <Kevin147444@gmail.com>
2023-03-22 14:28:17 -0400

    Series de kevin + Intefaz creada

---
+++ Proyecto2Operativos/src/Clases/SerieKevin.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author kevin
+ */
+public class SerieKevin {
+    int id;
+    int prioridad;
+    int contador;
+
+    public SerieKevin(int id, int prioridad, int contador) {
+        this.id = id;
+        this.prioridad = prioridad;
+        this.contador = contador;
+    }
+}
