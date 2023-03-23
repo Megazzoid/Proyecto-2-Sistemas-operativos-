234cc8e3921e19b31ebc1b4881c8fe1ebf4c0683
D-Lopez21 <d.lopez@correo.unimet.edu.ve>
2023-03-23 00:14:22 -0400

    Serie, Cola, conexion interfaz y admin Dirgo

---
+++ Proyecto2Operativos/src/Clases/SerieDiego.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author danma
+ */
+public class SerieDiego {
+    private int id;
+    private int prio;
+    private int contador;
+
+    public SerieDiego(int id, int prio, int contador) {
+        this.id = id;
+        this.prio = prio;
+        this.contador = contador;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrio() {
+        return prio;
+    }
+
+    public void setPrio(int prio) {
+        this.prio = prio;
+    }
+
+    public int getContador() {
+        return contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+    
+}
