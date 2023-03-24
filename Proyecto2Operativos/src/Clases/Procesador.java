7e1337092021fc2a51d4d1b9a4b73ed5763283ab
D-Lopez21 <d.lopez@correo.unimet.edu.ve>
2023-03-23 21:52:41 -0400

    Unido revisar series

--- Proyecto2Operativos/src/Clases/Procesador.java
+++ Proyecto2Operativos/src/Clases/Procesador.java
@@ -20,7 +20,8 @@ public class Procesador {
         
     }
 
-    public void RevisarSeries(QueueKevin QueueKevin1, QueueKevin QueueKevin2, QueueKevin QueueKevin3, QueueKevin QueueKevinRefuerzo) {
+    public void RevisarSeries(QueueKevin QueueKevin1, QueueKevin QueueKevin2, QueueKevin QueueKevin3, QueueKevin QueueKevinRefuerzo,
+            ColaDiego ColaDiego1,ColaDiego ColaDiego2,ColaDiego ColaDiego3,ColaDiego ColaDiegoRefuerzo) {
 
         int prob = new Random().nextInt(100);
      
@@ -40,16 +41,28 @@ public class Procesador {
 
                     }else if(!QueueKevin3.isVacio()){
                         SerieKevin temp = QueueKevin3.extraer();
-                        QueueKevin3.insertar(temp);
+                        QueueKevin3.insertar(temp);    
+                    } 
+                    else if(!ColaDiego1.isEmpty()){
+                        SerieDiego temp = ColaDiego1.Desencolar();
+                        ColaDiego1.Encolar(temp);
 
-                       
+                    }
+                        else if (!ColaDiego2.isEmpty()){
+                        SerieDiego temp = ColaDiego2.Desencolar();
+                        ColaDiego2.Encolar(temp);
+
+                    }else if(!ColaDiego3.isEmpty()){
+                        SerieDiego temp = ColaDiego3.Desencolar();
+                        ColaDiego3.Encolar(temp);               
                     }
                     else{
                         System.out.println("No paso"); 
                     }
                 
             } else {
-                    if(!QueueKevin1.isVacio()){
+                    
+                if(!QueueKevin1.isVacio()){
                         SerieKevin temp = QueueKevin1.extraer();
                         temp.prioridad = 4;
                         
@@ -68,69 +81,30 @@ public class Procesador {
                         temp.prioridad = 4;
                         
                         QueueKevinRefuerzo.insertar(temp);
+ 
+                    } else if(!ColaDiego1.isEmpty()){
+                        SerieDiego temp = ColaDiego1.Desencolar();
+                        temp.prio = 4;
+                        ColaDiegoRefuerzo.Encolar(temp);
+                    } else if (!ColaDiego2.isEmpty()){
+                        SerieDiego temp = ColaDiego2.Desencolar();
+                        temp.prio = 4;
+                        
+                        ColaDiegoRefuerzo.Encolar(temp);
 
-                       
+                    }else if(!ColaDiego3.isEmpty()){
+                        SerieDiego temp = ColaDiego3.Desencolar();
+                        temp.prio = 4;
+                        
+                        ColaDiegoRefuerzo.Encolar(temp);
                     }
+                        
                     else{
                         System.out.println("No paso"); 
                     }
             }  
             
         }
-    
-    public void RevisarDiego(ColaDiego ColaDiego1,ColaDiego ColaDiego2,ColaDiego ColaDiego3,ColaDiego ColaDiegoRefuerzo){
-        
-        int prob = new Random().nextInt(100);
-     
-            if (prob < 40) {
-      
-            } else if (prob >= 40 && prob <= 67) {
-                if(!ColaDiego1.isEmpty()){
-                        SerieDiego temp = ColaDiego1.Desencolar();
-                        ColaDiego1.Encolar(temp);
-
-                    }
-                    else if (!ColaDiego2.isEmpty()){
-                        SerieDiego temp = ColaDiego2.Desencolar();
-                        ColaDiego2.Encolar(temp);
-
-                    }else if(!ColaDiego3.isEmpty()){
-                        SerieDiego temp = ColaDiego3.Desencolar();
-                        ColaDiego3.Encolar(temp);               
-                    }
-                    else{
-                        System.out.println("No paso"); 
-                    }
-                
-            } else {
-                    if(!ColaDiego1.isEmpty()){
-                        SerieDiego temp = ColaDiego1.Desencolar();
-                        temp.prio = 4;
-                        
-                        ColaDiegoRefuerzo.Encolar(temp);
-
-                    }
-                    else if (!ColaDiego2.isEmpty()){
-                        SerieDiego temp = ColaDiego2.Desencolar();
-                        temp.prio = 4;
-                        
-                        ColaDiegoRefuerzo.Encolar(temp);
-
-
-                    }else if(!ColaDiego3.isEmpty()){
-                        SerieDiego temp = ColaDiego3.Desencolar();
-                        temp.prio = 4;
-                        
-                        ColaDiegoRefuerzo.Encolar(temp);
-
-                       
-                    }
-                    else{
-                        System.out.println("No paso"); 
-                    }
-            } 
-    }
-        
         
     }
 
