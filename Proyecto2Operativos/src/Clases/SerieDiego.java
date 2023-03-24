04bbdd883928e7db404eeeead21f6cc301fc89ec
D-Lopez21 <d.lopez@correo.unimet.edu.ve>
2023-03-23 21:25:43 -0400

    arreglos

--- Proyecto2Operativos/src/Clases/SerieDiego.java
+++ Proyecto2Operativos/src/Clases/SerieDiego.java
@@ -13,11 +13,27 @@ public class SerieDiego {
     int id;
     int prio;
     int contador;
+    String intro; 
+    String inicio; 
+    String cierre;
+    String creditos;
+    int calidad;
+    String personaje;
+    String episodio;
+    int tiempo;
 
-    public SerieDiego(int id, int prio, int contador) {
+    public SerieDiego(int id, int prio, int contador,String intro,String inicio, String cierre,String creditos, int calidad,String personaje,String episodio,int tiempo) {
         this.id = id;
         this.prio = prio;
         this.contador = contador;
+        this.inicio = inicio;
+        this.intro = intro;
+        this.cierre = cierre;
+        this.creditos = creditos;
+        this.calidad = calidad;
+        this.personaje = personaje;
+        this.episodio = episodio;
+        this.tiempo = tiempo;
     }
 
     public int getId() {
