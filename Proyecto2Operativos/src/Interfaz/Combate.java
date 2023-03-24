18b9f38118e26d681efb96280aab68163d3b65d4
D-Lopez21 <d.lopez@correo.unimet.edu.ve>
2023-03-23 23:04:38 -0400

    Pelea

--- Proyecto2Operativos/src/Interfaz/Combate.java
+++ Proyecto2Operativos/src/Interfaz/Combate.java
@@ -28,27 +28,65 @@ public class Combate extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Estudio2Pelea = new javax.swing.JTextField();
+        Estudio1Pelea = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        Estudio2Pelea.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Estudio2PeleaActionPerformed(evt);
+            }
+        });
+        getContentPane().add(Estudio2Pelea, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 360, 240, 40));
+
+        Estudio1Pelea.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Estudio1PeleaActionPerformed(evt);
+            }
+        });
+        getContentPane().add(Estudio1Pelea, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 360, 240, 40));
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 2, 48)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel2.setText("Listos para el combate!");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 20, -1, -1));
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 2, 48)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel3.setText("Estudio 1");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 290, -1, -1));
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 2, 48)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel4.setText("Estudio 2");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 290, -1, -1));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/rick-and-morty-ready-to-fight-zp7lpaxg59eecitk.gif"))); // NOI18N
         jLabel1.setText(".");
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 887, javax.swing.GroupLayout.PREFERRED_SIZE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 887, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void Estudio2PeleaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Estudio2PeleaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_Estudio2PeleaActionPerformed
+
+    private void Estudio1PeleaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Estudio1PeleaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_Estudio1PeleaActionPerformed
+ public void setTextColaDiegoPelea(String text){
+        Estudio2Pelea.setText(text);
+    }
+  public void setTextColaKevinPelea(String text){
+        Estudio1Pelea.setText(text);
+    }
     /**
      * @param args the command line arguments
      */
@@ -85,6 +123,11 @@ public class Combate extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField Estudio1Pelea;
+    private javax.swing.JTextField Estudio2Pelea;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     // End of variables declaration//GEN-END:variables
 }
